6ad556637a8d48a9b4445ed3817325ab63794e00
Yael <yael@yael-VirtualBox.(none)>
2017-12-04 23:50:10 +0200

    Server

---
+++ Server.h
@@ -0,0 +1,39 @@
+/*
+ * Server.h
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Server {
+	/*
+	 * This server handles connecting two clients to one
+	 * another
+	 */
+
+private:
+	int port;
+	int serverSocket;
+
+public:
+	Server(int port);
+	/*
+	 * Getting the port from file
+	 */
+	Server(char* fileName);
+	void start();
+	void stop();
+};
+
+
+
+#endif /* SERVER_H_ */
