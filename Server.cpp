b7eaf3a1c58cddf824af071cb7f26c038bf33f1e
Roni Fultheim <ronif10@gmai.com>
2017-12-05 17:19:20 +0200

    changed reading from file to using >>/<<\nadded config file\nchanged passing messages to sockets to use read/write and send integers (left original in a comment)\nin binding socket 2 - debugged to correct cliend variables (copy and paste problem)\nadded client socket descriptors swapping mechanism at end of small loop

--- Server.cpp
+++ Server.cpp
@@ -1,10 +1,6 @@
 /*
  * server.cpp
- *
- *  Created on: Dec 4, 2017
- *      Author: yael
  */
-
 
 #include "Server.h"
 
@@ -26,7 +22,9 @@ Server::Server(int port): port(port), serverSocket(0){
 }
 
 Server::Server(char* fileName): serverSocket(0){
-	string buffer,dummyLine;
+	//TODO - recommendation - use my code
+
+	/*string buffer,dummyLine;
 	ifstream config;
 	config.open(fileName, std::fstream::in);
 	getline(config, dummyLine);
@@ -38,7 +36,26 @@ Server::Server(char* fileName): serverSocket(0){
 	buffer.erase(buffer.begin(), buffer.begin() + strlen("Port = "));
 
 	port = atoi(buffer.c_str());
+	config.close();*/
+
+	ifstream config;
+	config.open("config.txt", std::fstream::in);
+
+	if (!config) {
+		cerr << "Unable to open Server configuration file";
+		exit(1);   // call system to stop
+	}
+
+	//read server's port from the configuration file
+	string portNum;
+	config >> portNum;
+
+	//close file
 	config.close();
+
+	//assign server port - 	translate port number to integer
+	port = atoi(portNum.c_str());
+
 }
 void Server::start(){
 	int client1_sd, client2_sd;
@@ -69,7 +86,7 @@ void Server::start(){
 
 	//binding
 	if (bind(serverSocket, (struct sockaddr* )&serverAddress,
-		sizeof(serverAddress)) == -1){
+			sizeof(serverAddress)) == -1){
 		throw "Error on binding";
 	}
 
@@ -80,49 +97,122 @@ void Server::start(){
 		cout<< "Waiting for connections";
 
 		//Accepting first client
-		client1_sd = accept(serverSocket,
-					(struct sockaddr* )&client1Address,
-				 	 &client1AddressLen);
+		client1_sd = accept(serverSocket, (struct sockaddr* )&client1Address, &client1AddressLen);
 		cout<< "Client 1 entered!";
 
 		//Sending 1 to to show him he is the first to enter
-		buffer[0] = '1';
-		send(client1_sd,buffer,1024,0);
+		//TODO - why not use write/read? then just send integers...
+		/*buffer[0] = '1';
+		send(client1_sd,buffer,1024,0);*/
+
+		int n = write(client1_sd, &(1), sizeof(1));
+		if (n == -1) {
+			cout << "Error writing to socket" << endl;
+			return;
+		}
 
 		//Accepting second client
-		client2_sd = accept(serverSocket,
-					(struct sockaddr* )&client1Address,
-					&client1AddressLen);
+		//TODO - should be address 2...
+		client2_sd = accept(serverSocket, (struct sockaddr* )&client2Address, &client2AddressLen);
 		cout<<"Client 2 entered!";
 
 		//Sending 2 to him to show him he is the second to enter
-		buffer[0] = '2';
-		send(client2_sd,buffer,1024,0);
+		//TODO - why not use write/read? then just send integers...
+		/*buffer[0] = '2';
+		send(client2_sd,buffer,1024,0);*/
+		n = write(client2_sd, &(2), sizeof(2));
+		if (n == -1) {
+			cout << "Error writing to socket" << endl;
+			return;
+		}
+
+		//declare row, column and temporary variable (avoid redeclaring at each iteration)
+		int row, column, temp_sd;
 
 		while(true){
+			/*
 			memset(&buffer[0], 0, sizeof(buffer));
 			//input - client 1
 			recv(client1_sd, buffer, 1024, 0);
-			if(strcmp(buffer, "End") == 0){
+			if(strcmp(buffer, "End") == 0){ //TODO - should be using: n = read(..), then if (n==-2) -> exit
 				close(client1_sd);
 				close(client2_sd);
 				break;
 			}
 
 			//return message
+			//TODO - why not use write/read? then just send integers...
 			send(client2_sd, buffer, 1024, 0);
 			memset(&buffer[0], 0, sizeof(buffer));
 
+
 			//input - client 2
+			//TODO - why not use write/read? then just send integers...
 			recv(client2_sd, buffer, 1024, 0);
-			if(strcmp(buffer, "End") == 0){
+			if(strcmp(buffer, "End") == 0){ //TODO - should be using: n = read(..), then if (n==-2) -> exit
 				close(client1_sd);
 				close(client2_sd);
 				break;
 			}
 
 			//return message
-			send(client1_sd, buffer, 1024, 0);
-		}
-	}
+			//TODO - why not use write/read? then just send integers...
+			send(client1_sd, buffer, 1024, 0);*/
 
+			//read row - first number sent
+			n = read(client1_sd, &row, sizeof(row));
+			if (n == -1) {
+				cout << "Error reading row from socket" << endl;
+				return;
+			}
+
+			//if game is over (-2) - close and wait for new clients
+			if(row == -2){
+				close(client1_sd);
+				close(client2_sd);
+				break;
+			}
+
+			//if other player had no moves (-1) - send that
+			if (row == -1) {
+				n = write(client2_sd, &row, sizeof(row));
+				if (n == -1) {
+					cout << "Error writing 'no moves' to socket" << endl;
+					return;
+				}
+			}
+			//else - other player made a move - read column of move and send it to opponent
+			else {
+				//read column
+				n = read(client1_sd, &column, sizeof(column));
+				if (n == -1) {
+					cout << "Error reading column from socket" << endl;
+					return;
+				}
+
+				//write row to opponent
+				n = write(client2_sd, &row, sizeof(row));
+				if (n == -1) {
+					cout << "Error writing row to socket" << endl;
+					return;
+				}
+
+				//write column to opponent
+				n = write(client2_sd, &column, sizeof(column));
+				if (n == -1) {
+					cout << "Error writing column' to socket" << endl;
+					return;
+				}
+			}
+
+			//switch players and keep on playing
+			//TODO - recognize that trick from someone?? ;)
+			temp_sd = client1_sd;
+			client1_sd = client2_sd;
+			client2_sd = temp_sd;
+
+		} //end small loop
+
+	} //end big loop
+
+} //end function
