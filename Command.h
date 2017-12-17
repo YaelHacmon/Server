6ca6bd07a6a0463f9ee5da33ad641adbb593f4d5
Roni Fultheim <ronif10@gmai.com>
2017-12-17 12:15:18 +0200

    created Command interface

---
+++ Command.h
@@ -0,0 +1,33 @@
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
+
+#include <vector>
+#include <string>
+
+/**
+ * Represents a command from the client (sender, creator) to the server (receiver, invoker)
+ */
+class Command {
+public:
+	//constructor
+	/**
+	 * Constructor taking the server to be executed on, by reference.
+	 * @param server to apply command to
+	 */
+	Command(Server &server);
+
+	//virtual destructor
+	virtual ~Command() {}
+
+	/**
+	 * Executes the action of this command.
+	 * @param args arguments for command's execution, by the set protocol of the certain instance
+	 */
+	virtual void execute(std::vector<std::string> args) = 0;
+
+protected:
+	Server server_;
+};
+
+#endif /* COMMAND_H_ */
---
+++ CommandManager.h
@@ -0,0 +1,14 @@
+/*
+ * CommandManager.h
+ *
+ *  Created on: Dec 17, 2017
+ *      Author: roni
+ */
+
+#ifndef COMMANDMANAGER_H_
+#define COMMANDMANAGER_H_
+
+
+
+
+#endif /* COMMANDMANAGER_H_ */
